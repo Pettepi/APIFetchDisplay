c4cbcdae623362a83bad0b5c7fb8cddd31ebe8c5
pp <petteripiironen@Petteri-MacBook-Pro.local>
2019-02-13 19:33:32 +0900

    Updates

---
+++ precache-manifest.40c91df23154c98cadaffe93917dbe5d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc145092b4292d9c1fb2",
+    "url": "/apifetchdisplay/static/js/main.cc145092.chunk.js"
+  },
+  {
+    "revision": "eaf91d9c8b6b72c57c51",
+    "url": "/apifetchdisplay/static/js/1.eaf91d9c.chunk.js"
+  },
+  {
+    "revision": "4e2024227478a07d939e",
+    "url": "/apifetchdisplay/static/js/runtime~main.4e202422.js"
+  },
+  {
+    "revision": "bca36c6f935ee87afa3f262f47f8abeb",
+    "url": "/apifetchdisplay/index.html"
+  }
+];
